3b89175781962b792af6e17d552c837358a01c1f
Ashish Papanai <ashishpapanai00@gmail.com>
2021-09-01 07:19:05 +0530

    TensorBoardX, GANs

---
+++ Deep_Learning/tensorboard_02.py
@@ -0,0 +1,15 @@
+import math
+from tensorboardX import SummaryWriter
+
+if __name__ == "__main__":
+    writer = SummaryWriter()
+    funcs = {"sin": math.sin, "cos": math.cos, "tan": math.tan}
+
+    for angle in range(-360, 360):
+        angle_rad = math.radians(angle)
+        for name, func in funcs.items():
+            val = func(angle_rad)
+            writer.add_scalar(name, val, angle)
+
+    writer.close()
+    
